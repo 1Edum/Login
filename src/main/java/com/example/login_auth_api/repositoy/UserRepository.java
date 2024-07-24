28e4a0edb7e0d5308cc5d032f20d55c2df7435d0
1Edum <eduardomatosdesouza2@gmail.com>
2024-07-24 08:09:10 -0300

    feat: uptade UserRepository

--- src/main/java/com/example/login_auth_api/repositoy/UserRepository.java
+++ src/main/java/com/example/login_auth_api/repositoy/UserRepository.java
@@ -1,8 +1,11 @@
 package com.example.login_auth_api.repositoy;
 
 import com.example.login_auth_api.domain.User;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
-
+    Optional<User> findByEmail(String email);
 }
